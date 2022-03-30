ca5150dccdebab8c391f3a96640e74d5c901d4c1
Josifek <100674201+VelkejKluk@users.noreply.github.com>
2022-03-30 14:15:31 +0200

    Add files via upload

---
+++ hokuspokus/hokuspokus/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.WriteLine("Toto je hrací kostka! \nKlikněte na enter a kostička se začne točit! ");
+
+
+while (true)
+{
+    Random randCisla = new Random();
+    int cisla = randCisla.Next(1, 7);
+    string input = Console.ReadLine().Trim();
+    
+    if (input == "")
+    {
+        Console.Clear();
+        Console.WriteLine(cisla);
+
+    }
+    else
+    {
+        Console.WriteLine("Neplatná hodnota... \nzadejte znovu: ");
+        break;
+    }
+}                           
+       
